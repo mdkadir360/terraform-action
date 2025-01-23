afcce044ae914e3d41899612d5c796f81787c70c
Abdul Kadir <101203829+mdkadir360@users.noreply.github.com>
2025-01-23 08:42:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
+   backend "azurerm" {
+    resource_group_name  = "myrg"
+    storage_account_name = "kadirstorage25"
+    container_name       = "vm-container"
+    key                  = "vm.terraform.tfstate"
   }
 }
 module "RG" {
